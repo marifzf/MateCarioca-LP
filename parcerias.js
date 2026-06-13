c8c32c41223fe497a88ae6fbbdce9fcb2ca69145
lucasfslopes <lukasmdf2@gmail.com>
2026-06-13 19:05:42 -0300

    ajuste design de elementos e formulario

--- parcerias.js
+++ parcerias.js
@@ -1,3 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const elemento = document.querySelector("#form-contato");
+    if(!elemento) return;
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+
+                elemento.style.visibility = 'visible';
+                elemento.style.animation = 'slideUp 0.8s ease forwards';
+                
+                observer.unobserve(entry.target);
+            }
+        });
+    }, {
+        threshold: 0.5
+    });
+
+    observer.observe(elemento);
+});
+
 document.addEventListener('submit', () => {
     const form = document.getElementById('form-contato');
 
